86ac147a90efa94586b9d020342ff06f8b35e8a1
Maruf Nadaf <153378078+MarufxAlchemist@users.noreply.github.com>
2026-03-18 23:07:33 +0530

    Update FavouriteBooks.tsx

--- src/components/books/FavouriteBooks.tsx
+++ src/components/books/FavouriteBooks.tsx
@@ -8,7 +8,7 @@ import BookSearch from "@/features/search/components/BookSearch";
 
 export default function FavouriteBooks() {
   const [books, setBooks] = useState<Book[]>([]); // State to store the list of books.
-  const [loading, setLoading] = useState(true); // Loading state
+  const [loading, setLoading] = useState(true); // Loading state.
 
   useEffect(() => {
     // Fetch all books on component mount.
